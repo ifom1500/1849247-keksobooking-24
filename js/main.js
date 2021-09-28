293bdef03bbdbac6869813a54499fc364d6a324c
ifom1500 <ifom1500@mail.ru>
2021-09-28 11:45:53 +0300

    Исправил замечания

--- js/main.js
+++ js/main.js
@@ -4,13 +4,10 @@ const getRandomFloat = (min, max, precision) => {
     min = max;
     max = swap;
   }
-
-  const numIncludeToRange = (precision === 0) ? 1 : (1 / 10 ** precision);
-  const num = min + Math.random() * (max + numIncludeToRange - min);
-  return Math.floor(num * 10 ** precision) / 10 ** precision;
+  return +(Math.random() * (max - min) + min).toFixed(precision);
 };
 
-const getRandomIntNumber = (min, max) => getRandomFloat(min, max, 0);
+const getRandomInteger = (min, max) => getRandomFloat(min, max, 0);
 
 getRandomFloat(1, 10, 2);
-getRandomIntNumber (20, 40);
+getRandomInteger (20, 40);
