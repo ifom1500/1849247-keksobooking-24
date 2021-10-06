4b22b0fc19e4ded9842b50fbe2aed9cb306b2529
ifom1500 <ifom1500@mail.ru>
2021-10-06 13:55:49 +0300

    Перенес функции рандомных чисел

--- js/main.js
+++ js/main.js
@@ -21,16 +21,19 @@ const PHOTOS = [
 
 const SIMILAR_AD_COUNT = 10;
 
-const getRandomFloat = (min, max, precision) => {
-  if (min > max) {
-    const swap = min;
-    min = max;
-    max = swap;
-  }
-  return +(Math.random() * (max - min) + min).toFixed(precision);
+const getRandomPositiveFloat = (a, b, precision) => {
+  const min = Math.min(Math.abs(a), Math.abs(b));
+  const max = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (max - min) + min;
+  return +result.toFixed(precision);
 };
 
-const getRandomInteger = (min, max) => getRandomFloat(min, max, 0);
+const getRandomPositiveInteger = (a, b) => {
+  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (max - min + 1) + min;
+  return Math.floor(result);
+};
 
 const createCounter = () => {
   let counter = 0;
@@ -42,15 +45,15 @@ const createCounter = () => {
 
 const counter = createCounter();
 
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
 const getRandomArrayWithoutRepeats = (array) => {
   const clone = array.slice();
   const newArray = [];
-  newArray.length = getRandomInteger(0, array.length);
+  newArray.length = getRandomPositiveInteger(0, array.length);
 
   for (let i = 0; i < newArray.length; i++) {
-    const index = getRandomInteger(0, clone.length - 1);
+    const index = getRandomPositiveInteger(0, clone.length - 1);
     const item = clone.splice(index, 1).join();
     newArray[i] = item;
   }
@@ -59,18 +62,18 @@ const getRandomArrayWithoutRepeats = (array) => {
 
 const getRandomArrayWithRepeats = (array, maxQuantity) => {
   const newArray = [];
-  newArray.length = getRandomInteger(0, maxQuantity);
+  newArray.length = getRandomPositiveInteger(0, maxQuantity);
 
   for (let i = 0; i < newArray.length; i++) {
-    const item = array[getRandomInteger(0, array.length - 1)];
+    const item = array[getRandomPositiveInteger(0, array.length - 1)];
     newArray[i] = item;
   }
   return newArray;
 };
 
 const createApartmentAd = () => {
-  const lat = getRandomFloat(35.65, 35.7, 5);
-  const lng = getRandomFloat(139.7, 139.8, 5);
+  const lat = getRandomPositiveFloat(35.65, 35.7, 5);
+  const lng = getRandomPositiveFloat(139.7, 139.8, 5);
 
   return {
     author: {
@@ -79,10 +82,10 @@ const createApartmentAd = () => {
     offer: {
       title: 'There’s no place like home',
       address: `${lat}, ${lng}`,
-      price: getRandomInteger(4000, 10000),
+      price: getRandomPositiveInteger(4000, 10000),
       type: getRandomArrayElement(APARTMENT_TYPES),
-      rooms: getRandomInteger(1, 9),
-      guests: getRandomInteger(1, 9),
+      rooms: getRandomPositiveInteger(1, 9),
+      guests: getRandomPositiveInteger(1, 9),
       checkin: getRandomArrayElement(WORKING_HOURS),
       checkout: getRandomArrayElement(WORKING_HOURS),
       features: getRandomArrayWithoutRepeats(FEATURES),
